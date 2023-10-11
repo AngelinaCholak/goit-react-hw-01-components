858d121ebfbf1043810364de8e332604aba64991
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-10-11 13:19:26 +0400

    HW-01react

---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import css from './profile.css';
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="avatar"
+        />
+              <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li className='with-border'>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li className='with-border'>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li className='with-border'>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile; 
