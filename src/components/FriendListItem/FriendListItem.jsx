bbe0e6f5e6b520f0c18b0141c994303c14eb636e
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-10-12 17:31:13 +0400

    hw-react

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import styles from './friendListItem.module.css';
 const FriendItem = ({ friend }) => (
-  <li className={styles.friendList}>
-    <span className={`status ${friend.isOnline ? 'online' : 'offline'}`}></span>
+  <li className={styles.friends}>
+    <span className={`${styles.status} ${friend.isOnline ? styles.online : styles.offline}`}></span>
         <img
             src={friend.avatar}
             alt="User avatar"
